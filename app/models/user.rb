f574831fbfd3c7960dd7957cc8b9a98eb2bc9556
maco <mako.ishikawa11.18@gmail.com>
2020-08-22 14:31:56 +0900

    Post モデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, uniqueness: true, length: { maximum: 256 }
+  validates :age, presence: true, length: { maximum: 150 }
 end
