3bcd7d6217c1b5aad0b8fd21f479a79604a58c7a
maco <mako.ishikawa11.18@gmail.com>
2020-08-22 18:17:52 +0900

    エラーメッセージでBootstrapのアラートを利用

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }
   validates :email, uniqueness: true, presence: true, length: { maximum: 256 }
-  validates :age, presence: true, length: { maximum: 150 }
+  validates :age, presence: true, inclusion: { in: 0..150 }
 end
